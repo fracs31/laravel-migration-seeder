fc63069bd03c02b2592bdb6c5e6a4f6aa8c00bcf
fracs31 <pist.fracs95@gmail.com>
2023-04-12 14:06:20 +0200

    ADD trains migration

--- database/migrations/2023_04_12_114855_create_trains_table.php
+++ database/migrations/2023_04_12_114855_create_trains_table.php
@@ -14,8 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); //id
+            $table->string("company"); //azienda
+            $table->string("start_station"); //stazione di partenza
+            $table->string("last_station"); //stazione di arrivo
+            $table->dateTime("start_time"); //orario di partenza
+            $table->dateTime("arrive_time"); //orario di arrivo
+            $table->string("train_code"); //codice del treno
+            $table->boolean("in_time"); //in orario
+            $table->boolean("cancelled"); //cancellato
+            $table->timestamps(); //data di creazione e modifica
         });
     }
 
