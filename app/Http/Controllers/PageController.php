ffad041ae38809073be11857bc8efa80c12ab4f6
fracs31 <pist.fracs95@gmail.com>
2023-04-12 15:10:33 +0200

    ADD train's cards and GET informations from the database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train; //model
 
 class PageController extends Controller
 {
     //Homepage
     public function homepage() {
-        return view("welcome"); //restituisco la vista
+        $trains = Train::all(); //prendo tutti i treni
+        $today = Train::where("start_time", "like", "2023-04-12%")->get(); //prendo i treni di oggi
+        //Dati
+        $data = [
+            "trains" => $trains,
+            "today" => $today
+        ];
+        return view("welcome", $data); //restituisco la vista
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,57 @@
 <body>
     {{-- Main --}}
     <main>
-        <h1>Treni</h1>
+        {{-- Container --}}
+        <div class="container">
+            {{-- Titolo --}}
+            <h1 class="text-center pt-5">Treni</h1>
+            <h2 class="pt-4">Tutti i treni</h2>
+            {{-- Riga --}}
+            <div class="row">
+                {{-- Ciclo --}}
+                @foreach ($trains as $train)
+                    {{-- Colonna --}}
+                    <div class="col">
+                        {{-- Carta --}}
+                        <div class="card" style="width: 18rem;">
+                            {{-- Codice del treno --}}
+                            <div class="card-header">
+                                {{ $train->train_code }}
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Stazione di partenza: {{ $train->start_station }}</li>
+                                <li class="list-group-item">Stazione di arrivo: {{ $train->last_station }}</li>
+                                <li class="list-group-item">Orario di partenza: {{ $train->start_time }}</li>
+                                <li class="list-group-item">Orario di arrivo: {{ $train->arrive_time }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <h2 class="pt-4">Treni di oggi</h2>
+            {{-- Riga --}}
+            <div class="row">
+                {{-- Ciclo --}}
+                @foreach ($today as $train)
+                    {{-- Colonna --}}
+                    <div class="col">
+                        {{-- Carta --}}
+                        <div class="card" style="width: 18rem;">
+                            {{-- Codice del treno --}}
+                            <div class="card-header">
+                                {{ $train->train_code }}
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Stazione di partenza: {{ $train->start_station }}</li>
+                                <li class="list-group-item">Stazione di arrivo: {{ $train->last_station }}</li>
+                                <li class="list-group-item">Orario di partenza: {{ $train->start_time }}</li>
+                                <li class="list-group-item">Orario di arrivo: {{ $train->arrive_time }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 </body>
 </html>
